3fd3ce9d80a1eae880805c70685c0af98620014a
Subhadeeps91 <subhadeeps91@gmail.com>
2025-07-23 18:24:46 +0200

    Node.js setup for cypress results

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,17 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   projectId: 'sxakze',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: true,
+    json: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // you can also plug in custom test logic here
     },
   },
-};
+});
--- cypress/e2e/sanity.cy.js
+++ cypress/e2e/sanity.cy.js
@@ -1,7 +1,7 @@
 describe('Sanity Check', () => {
   it('opens the portfolio site', () => {
     cy.log('Test has started!')
-    cy.visit('https://example.com')
+    cy.visit('https://subhadeeps91.github.io/')
     cy.title().should('exist')
   })
 })
