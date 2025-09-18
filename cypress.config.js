daec98d02b9e1b14f3f7e59d2d056ce4801ca3c4
Subhadeeps91 <subhadeeps91@gmail.com>
2025-09-18 16:53:38 +0200

    Updating package and config file for Cypress report generation

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'sxakze',
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'cypress/results',
     overwrite: false,
     html: true,
-    json: true,
+    json: true
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // you can also plug in custom test logic here
+      // custom logic if needed
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}'
   },
 });
